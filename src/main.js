766ced9c2787d9f111c41eb6387f6c148530c005
Alejandro Aranguren <alenguren@hotmail.com>
2024-06-13 17:11:25 -0500

    config

--- src/main.js
+++ src/main.js
@@ -46,6 +46,22 @@ k.scene("main", async () => {
         },
         "player",
     ]);
+
+    for (const layer of layers){
+        if (layer.name === "boundaries") {
+            for (const boundary of layer.objects) {
+                map.add([
+                    k.area({
+                        shape: new k.rect(k.vec2(0), boundary.width, boundary.height),
+                    }),
+                    k.body({ isStatic: true }),
+                    k.pos(boundary.x, boundary.y,
+                    boundary.name
+                    )
+                ]);
+            }
+        }
+    }
 });
 
 k.go("main");
