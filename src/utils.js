3e94e59567b2d352d5bb1104c0ceaa1d4805cb91
Alejandro Aranguren <alenguren@hotmail.com>
2024-06-16 13:30:11 -0500

    text logic

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,6 @@
 export function displayDialogue(Text, onDisplayEnd) {
     const dialogueUI = document.getElementById("textbox-container");
+    const dialogue = document.getElementById("dialogue");
+
+    dialogueUI.style.display = "block";
 }
