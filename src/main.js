9c0805ad1606cd4e0c936ae1f4de14ecba91839e
Alejandro Aranguren <alenguren@hotmail.com>
2024-06-16 15:31:42 -0500

    animation

--- src/main.js
+++ src/main.js
@@ -127,21 +127,19 @@ k.scene("main", async () => {
                 return;
         }
 
-        if (Math.abs(mouseAngle) > upperBoun) {
+        if (Math.abs(mouseAngle) > upperBound) {
             player.flipX = false;
-            if (player.curAnim () !== "walk-side") player-play("walk-side");
+            if (player.curAnim() !== "walk-side") player.play("walk-side");
             player.direction = "right";
             return;
         }
 
         if (Math.abs(mouseAngle) < lowerBound) {
             player.flipX = true;
-            if (player.curAnim () !== "walk-side") player-play("walk-side");
+            if (player.curAnim() !== "walk-side") player.play("walk-side");
             player.direction = "left";
             return;
         }
-
-        })
     });
 });
 
